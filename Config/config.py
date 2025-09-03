1f4f89105ff258f1c20be9f1276942d018d2b090
Laura Sophie Rivera Martin <sophie@MacBook-Air-de-Laura.local>
2025-09-03 00:28:37 -0500

    main

--- Config/config.py
+++ Config/config.py
@@ -2,5 +2,5 @@ class Config:
     """
     Clase de configuración para rutas y parámetros del ETL.
     """
-    INPUT_PATH = '/Extract/Files/mobile_game_inapp_purchases.csv' 
-    OUTPUT_PATH = '/Extract/Files/output_clean.csv'
+    INPUT_PATH = 'Extract/Files/mobile_game_inapp_purchases.csv'
+    OUTPUT_PATH = 'Extract/Files/mobile_game_inapp_purchases_clean.csv'
---
+++ main.py
@@ -0,0 +1,23 @@
+from Config.config import Config
+from Extract.extractor import Extractor
+from Transform.transformer import Transformer
+from Load.loader import Loader
+
+def main():
+    # Extraer datos
+    extractor = Extractor(Config.INPUT_PATH)
+    raw_data = extractor.extract()
+
+    if raw_data is not None:
+        # Transformar datos
+        transformer = Transformer(raw_data)
+        cleaned_data = transformer.clean()
+
+        # Cargar datos
+        loader = Loader(cleaned_data)
+        loader.to_csv(Config.OUTPUT_PATH)
+    else:
+        print("No se pudo extraer el archivo de datos.")
+
+if __name__ == "__main__":
+    main()
